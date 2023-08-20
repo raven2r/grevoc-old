e572afac4224795fe1f80a94aee074c723552022
raven1 <raven1@runbox.me>
2023-08-20 23:16:36 +0200

    Added skeleton function to read config in Core class

--- grevoc-core/src/main/java/me/raven/grevoc/core/Core.java
+++ grevoc-core/src/main/java/me/raven/grevoc/core/Core.java
@@ -1,4 +1,10 @@
 package me.raven.grevoc.core;
 
 public class Core {
+    /** Reads config from .ini file
+     * @return true on success, false otherwise
+     */
+    public boolean readConfig() {
+        return false;
+    }
 }
